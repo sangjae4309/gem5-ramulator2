c213199008b608204dfbec53c669691b4b0f64e2
sangjae4309 <mainpower4309@gmail.com>
2024-01-13 13:05:37 +0000

    fix bug

--- mem/ramulator2.cc
+++ mem/ramulator2.cc
@@ -131,7 +131,7 @@ Ramulator2::tick()
     }
 
     schedule(tickEvent,
-        curTick() + ramulator2_memorysystem->get_tCK() * sim_clock::as_int::ns);
+        curTick() + ramulator2_memorysystem->get_tCK() * sim_clock::as_float::ns);
 }
 
 Tick
@@ -180,7 +180,7 @@ Ramulator2::recvTimingReq(PacketPtr pkt)
     {
         // Generate ramulator READ request and try to send to ramulator's memory system
         enqueue_success = ramulator2_frontend->
-            receive_external_requests(0, pkt->getAddr(), pkt->req->contextId(), 
+            receive_external_requests(0, pkt->getAddr(), 0 
             [this](Ramulator::Request& req) {
                 DPRINTF(Ramulator2, "Read to %ld completed.\n", req.addr);
                 auto& pkt_q = outstandingReads.find(req.addr)->second;
